3e7b83cfe08273be56b3c63237a0eeae4f60ae85
Yakisobapanman <110naokiti0527@gmail.com>
2018-06-20 11:10:22 +0900

    add

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 変数の定義 */////////////////////////////////////////////////////////////////////
-  $setchar='utf-8';/* 日本語文字セット UTF-8, Shift_JIS, EUC-JP,ISO-2022-JP */
+  $setchar='UTF8';/* 日本語文字セット UTF-8, Shift_JIS, EUC-JP,ISO-2022-JP */
   $pdate=$date=date("<b>Y/m/d H:i </b>");/* 日時自動記入するとき設定 */
   $lmt=100;/* 投稿文字制限 */
   $kugiri='<hr style="border:1px solid #eee">';/* 記事の区切り */
@@ -9,7 +9,7 @@
 
   mb_language("Japanese");
   ini_set('mbstring.internal_encoding',$setchar);
-  if(preg_match('/utf/i',$setchar))$charcode='utf-8';
+  if(preg_match('/utf/i',$setchar))$charcode='UTF8';
   if(preg_match('/shift|sjis/i',$setchar))$charcode='sjis';
   if(preg_match('/euc/i',$setchar))$charcode='eucjp';
   if(preg_match('/^jis|2022/i',$setchar))$charcode='jis';
